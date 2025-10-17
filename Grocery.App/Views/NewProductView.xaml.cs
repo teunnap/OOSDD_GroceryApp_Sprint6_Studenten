b79324a25ebe1822b7221357b3ef21363f2ee02a
Teun <teun.nap@windesheim.nl>
2025-10-17 13:57:43 +0200

    create NewProductView and viewmodel

---
+++ Grocery.App/Views/NewProductView.xaml.cs
@@ -0,0 +1,14 @@
+using Grocery.App.ViewModels;
+
+namespace Grocery.App.Views;
+
+public partial class NewProductView : ContentPage
+{
+    public NewProductView(NewProductViewModel viewModel)
+    {
+        InitializeComponent();
+        BindingContext = viewModel;
+    }
+}
+
+
